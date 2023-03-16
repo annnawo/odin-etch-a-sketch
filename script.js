1eeadd8bcfb8454580a8d7f8050326254b7fa4bc
anna woodward <annagracewoodward.com>
2023-03-16 16:06:03 -0500

    Work on initial div creation function

--- script.js
+++ script.js
@@ -1,6 +1,27 @@
 const slider = document.querySelector('.slider');
+let sliderValue = slider.value;
+
+const gridContainer = document.querySelector('#gridContainer');
+// const gridHolding = document.createElement('div');
+function gridCreation(sliderValue) {
+    const gridHolding = document.createElement('div');
+    for (let i=0; i < (sliderValue*sliderValue); i++) {
+        const gridDiv = document.createElement('div');
+        console.log(`here`);
+        gridDiv.style.cssText = 'border-style: dotted; border-width: 10px; border-color: black';
+        gridHolding.appendChild(gridDiv);
+    } 
+    gridContainer.appendChild(gridHolding);
+}
+
+gridCreation(sliderValue);
+
 slider.addEventListener('click', () => {
-    let sliderValue = slider.value;
+    newSliderValue = slider.value;
     console.log(sliderValue);
-    return sliderValue;
+    if (newSliderValue !== sliderValue) {
+        gridContainer.removeChild(gridHolding);
+        gridCreation(sliderValue);
+        return sliderValue;
+    }
 } );
