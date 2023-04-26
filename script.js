708e07912f7c988a91d83f51309e0a6cf7202aa7
anna woodward <annagracewoodward.com>
2023-04-26 11:01:03 -0500

    Add event listeners to change color on mouseover

--- script.js
+++ script.js
@@ -1,24 +1,12 @@
 const slider = document.querySelector('.slider');
 let sliderValue = slider.value;
 
-// const gridContainer = document.querySelector('#gridContainer');
-// // const gridHolding = document.createElement('div');
-// function gridCreation(sliderValue) {
-//     const gridHolding = document.createElement('div');
-//     for (let i=0; i < (sliderValue*sliderValue); i++) {
-//         const gridDiv = document.createElement('div');
-//         console.log(`here`);
-//         gridDiv.style.cssText = 'border-style: solid; border-width: 1px; border-color: black';
-//         gridHolding.appendChild(gridDiv);
-//     } 
-//     gridContainer.appendChild(gridHolding);
-// }
-
 let gridContainer = document.querySelector('#gridContainer');
 let gridHolding = document.createElement('div');
 gridHolding.setAttribute('id', 'canvas');
 gridHolding.className = 'canvasGridTotal';
 let initial = 0;
+let lastCycle = 0;
 
 let r = document.querySelector(':root');
 
@@ -28,36 +16,37 @@ let r = document.querySelector(':root');
 gridCreation(sliderValue);
 
 function gridCreation(sliderValue) {
-    //let gridDivs = gridHolding.querySelectorAll('blocks');
     let gridTest = gridHolding.childElementCount;
     if (gridTest !== 0) {
-        // console.log(gridTest);
         for (let j=0; j < gridTest; j++) {
             gridHolding.removeChild(gridHolding.childNodes[0]);
     }}
     r.style.setProperty('--colNum', sliderValue);
-    // let rs = getComputedStyle(r);
-    // console.log(rs.getPropertyValue('--rowNum'));
+    lastCycle = sliderValue*sliderValue;
     for (let i=0; i < (sliderValue*sliderValue); i++) {
-        const gridDiv = document.createElement('div');
+        let gridDiv = document.createElement('div');
         gridDiv.className = 'blocks';
+        gridDiv.setAttribute('id', '${i}');
+        gridDiv.addEventListener('mouseover', () => {    ///t
+                   gridDiv.style.cssText = 'background-color: yellow';  ///t
+        });
         gridDiv.style.cssText = 'border-style: solid; border-width: 1px; border-color: black';
+        gridHolding.appendChild(gridDiv);
         gridHolding.appendChild(gridDiv);
     } 
     gridContainer.appendChild(gridHolding);
 }
 
-// function gridDeletion(gridHolding) {
-//     gridContainer
-// }
+
 
 slider.addEventListener('click', () => {
     let newSliderValue = slider.value;
-    // console.log(newSliderValue);
     if (sliderValue !== newSliderValue) {
-    // if (newSliderValue !== sliderValue) {
-    //     gridContainer.removeChild(gridHolding);
     gridCreation(newSliderValue);
     sliderValue = slider.value;    
     }
 } );
+
+
+gridHolding.style.cssText = 'background-color: pink';
+
