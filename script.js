55e3e404525ea0b852aed2e3a43c4cd9977b159e
anna woodward <annagracewoodward.com>
2023-05-11 11:51:45 -0500

    Add ink color change functionality

--- script.js
+++ script.js
@@ -10,6 +10,8 @@ let lastCycle = 0;
 
 let r = document.querySelector(':root');
 
+let inkColor = '#000000';
+
 // const gridVar = window.getComputedStyle(document.querySelector('html'));
 // let rowNum = parseInt(gridVar.getPropertyValue('--rowNum'));
 
@@ -23,18 +25,13 @@ function gridCreation(sliderValue) {
     }}
     r.style.setProperty('--colNum', sliderValue);
     lastCycle = sliderValue*sliderValue;
-    for (let i=0; i < (sliderValue*sliderValue); i++) {
+    for (let i=0; i < lastCycle; i++) {
         let gridDiv = document.createElement('div');
         gridDiv.className = 'blocks';
         gridDiv.setAttribute('id', `${i}`);
-        // gridDiv.addEventListener('mouseover', () => {    
-                //    gridDiv.style.cssText = 'background-color: yellow';  
-        // });
-
-        gridDiv.addEventListener('mouseover', function() {
-            setInkColor(i);
-        });   
-
+        gridDiv.addEventListener('mouseover', () => {    
+            gridDiv.style.cssText = `background-color: ${inkColor}`;  
+        });
         gridDiv.style.cssText = 'border-style: solid; border-width: 1px; border-color: black';
         gridHolding.appendChild(gridDiv);
     } 
@@ -51,23 +48,80 @@ slider.addEventListener('click', () => {
 
 const clearButton = document.querySelector('#clearButton');
 clearButton.addEventListener('click', () => {
-    console.log('click');
     for (let j=0; j<lastCycle; j++) {
         let gridDivSelect = document.getElementById(j);
-        gridDivSelect.style.cssText = 'background-color: pink';  
-}
+        gridDivSelect.style.cssText = 'background-color: #f6f6f6';     
+    }
 });
 
-//gridHolding.style.cssText = 'background-color: pink';
+let blackInk = document.querySelector('#blackOptionI').addEventListener('click',  () => {
+    inkColor = '#000000';
+    for (let j=0; j<lastCycle; j++) {
+    setInk(inkColor, j);
+}});
 
+let gradientInk = document.querySelector('#gradientOptionI').addEventListener('click',  () => {
+    for (let j=0; j<lastCycle; j++) {
+        if (j%9 === 0) {
+            inkColor = '#e2d5df';
+        } 
+        else if (j%9 === 1) {
+            inkColor = '#e5adca';
+        } 
+        else if (j%9 === 2) {
+            inkColor = '#e9d5d8';
+        } 
+        else if (j%9 === 3) {
+            inkColor = '#e7c7d0';
+        } 
+        else if (j%9 === 4) {
+            inkColor = '#d5c4e4';
+        } 
+        else if (j%9 === 5) {
+            inkColor = '#ebabb3';
+        } 
+        else if (j%9 === 6) {
+            inkColor = '#d7b2dc';
+        } 
+        else if (j%9 === 7) {
+            inkColor = '#e59bcc';
+        } 
+        else if (j%9 === 8) {
+            inkColor = '#ccb4db';
+        } 
+        else if (j%9 === 8) {
+            inkColor = '#f4bac1';
+        } 
+        setInk(inkColor, j);
+}})  
 
+let randomInk = document.querySelector('#randomOptionI').addEventListener('click',  () => {
+    for (let j=0; j<lastCycle; j++) {
+        let newColor ='#';
+        for (let m=0; m<6; m++) {
+           newColor += Math.floor(Math.random()*10);
+        }
+        inkColor = newColor;
+        newColor = '#';
+        setInk(inkColor, j);
+}});
 
-// colors
+// function setInkColor(tile, colorI) {
+//     let gridSelect = document.getElementById(tile);
+//     gridSelect.style.cssText = 'background-color: pink';
+// }
 
-// let blackInk = document.querySelector('#blackOptionI').addEventListener('click', setInkColor);
-let inkColor;
+// function setInk(colorI) {
+//     for (let j=0; j<lastCycle; j++) {
+//         let gridDivSelect = document.getElementById(j);
+//         gridDivSelect.addEventListener('mouseover', function() {
+//             gridDivSelect.style.cssText = `background-color: ${colorI}`;
+//         });  
+// }}
 
-function setInkColor(tile) {
-    let gridSelect = document.getElementById(tile);
-    gridSelect.style.cssText = 'background-color: pink';
+function setInk(colorI, k) {
+        let gridDivSelect = document.getElementById(k);
+        gridDivSelect.addEventListener('mouseover', function() {
+        gridDivSelect.style.cssText = `background-color: ${colorI}`;
+});  
 }
