16a398d82fcee9b91f6b6c0faf7c2131bb3f29d2
anna woodward <annagracewoodward.com>
2023-09-02 00:03:12 -0500

    Refactor and housekeeping

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 const slider = document.querySelector('.slider');
 let sliderValue = slider.value;
 
-let gridContainer = document.querySelector('#gridContainer');
+let gridContainer = document.querySelector('#grid-container');
 let gridHolding = document.createElement('div');
 gridHolding.setAttribute('id', 'canvas');
-gridHolding.className = 'canvasGridTotal';
+gridHolding.className = 'canvas-grid-total';
 let initial = 0;
 let lastCycle = 0;
 
@@ -13,11 +13,9 @@ let r = document.querySelector(':root');
 let inkColor = '#000000';
 let canvasColor = '#f6f6f6';
 
-// const gridVar = window.getComputedStyle(document.querySelector('html'));
-// let rowNum = parseInt(gridVar.getPropertyValue('--rowNum'));
-
 gridCreation(sliderValue);
 
+// Populates the grid
 function gridCreation(sliderValue) {
     let gridTest = gridHolding.childElementCount;
     if (gridTest !== 0) {
@@ -33,12 +31,13 @@ function gridCreation(sliderValue) {
         gridDiv.addEventListener('mouseover', () => {    
             gridDiv.style.cssText = `background-color: ${inkColor}`;  
         });
-        gridDiv.style.cssText = `border-style: solid; border-width: 1px; border-color: black; background-color: ${canvasColor};`;
+        // gridDiv.style.cssText = `border-style: solid; border-width: 1px; border-color: black; background-color: ${canvasColor};`;
         gridHolding.appendChild(gridDiv);
     } 
     gridContainer.appendChild(gridHolding);
 }
 
+// Resizes the grid when the 'grid dimensions' slider is changed
 slider.addEventListener('click', () => {
     let newSliderValue = slider.value;
     if (sliderValue !== newSliderValue) {
@@ -47,20 +46,21 @@ slider.addEventListener('click', () => {
     }
 } );
 
-const clearButton = document.querySelector('#clearButton').addEventListener('click', () => {
+// Reverts the color of all grid boxes to the canvas color when the clear button is pressed
+const clearButton = document.querySelector('#clear-button').addEventListener('click', () => {
     for (let j=0; j<lastCycle; j++) {
         let gridDivSelect = document.getElementById(j);
         gridDivSelect.style.cssText = `background-color: ${canvasColor}`;     
     }
 });
 
-let blackInk = document.querySelector('#blackOptionI').addEventListener('click',  () => {
+let blackInk = document.querySelector('#black-option-1').addEventListener('click',  () => {
     inkColor = '#000000';
     for (let j=0; j<lastCycle; j++) {
     setInk(inkColor, j);
 }});
 
-let gradientInk = document.querySelector('#gradientOptionI').addEventListener('click',  () => {
+let gradientInk = document.querySelector('#gradient-option-1').addEventListener('click',  () => {
     for (let j=0; j<lastCycle; j++) {
         if (j%9 === 0) {
             inkColor = '#e2d5df';
@@ -95,37 +95,8 @@ let gradientInk = document.querySelector('#gradientOptionI').addEventListener('c
         setInk(inkColor, j);
 }})  
 
-// let randomInk = document.querySelector('#randomOptionI').addEventListener('click',  () => {
-//     for (let j=0; j<lastCycle; j++) {
-//         let newColor ='#';
-//         for (let m=0; m<6; m++) {
-//            let colInt = Math.floor(Math.random()*7);
-//            if (colInt === 1) {
-//             newColor += 'a';
-//            }
-//            else if (colInt === 2) {
-//             newColor += 'b';
-//            }
-//            else if (colInt === 3) {
-//             newColor += 'c';
-//            }
-//            else if (colInt === 4) {
-//             newColor += 'd';
-//            }
-//            else if (colInt === 5) {
-//             newColor += 'e;'
-//            }
-//            else {
-//             newColor += 'f';
-//            }
-           
-//         }
-//         inkColor = newColor;
-//         newColor = '#';
-//         setInk(inkColor, j);
-// }});
 let newColor ='#';
-let randomInk = document.querySelector('#randomOptionI').addEventListener('click',  () => {
+let randomInk = document.querySelector('#random-option-1').addEventListener('click',  () => {
     for (let j=0; j<lastCycle; j++) {
         newColor ='#';
         randomizeColor();
@@ -136,6 +107,7 @@ let randomInk = document.querySelector('#randomOptionI').addEventListener('click
     }
 });
 
+// Generates a new hex code
 function randomizeColor() {
     newColor = '#';
     for (let m=0; m<6; m++) {
@@ -159,48 +131,19 @@ function randomizeColor() {
          newColor += 'f';
 }}}
 
-//     for (let j=0; j<lastCycle; j++) {
-//         let newColor ='#';
-//         for (let m=0; m<6; m++) {
-//            let colInt = Math.floor(Math.random()*7);
-//            if (colInt === 1) {
-//             newColor += 'a';
-//            }
-//            else if (colInt === 2) {
-//             newColor += 'b';
-//            }
-//            else if (colInt === 3) {
-//             newColor += 'c';
-//            }
-//            else if (colInt === 4) {
-//             newColor += 'd';
-//            }
-//            else if (colInt === 5) {
-//             newColor += 'e;'
-//            }
-//            else {
-//             newColor += 'f';
-//            }
-           
-//         }
-//         inkColor = newColor;
-//         newColor = '#';
-//         setInk(inkColor, j);
-// }});
-
-let blackCanvas = document.querySelector('#blackOptionG').addEventListener('click', () => {
+let blackCanvas = document.querySelector('#black-option-g').addEventListener('click', () => {
     canvasColor = '#000000';
     for (let l=0; l<lastCycle; l++) {
         setCanvasC(canvasColor, l);
 }});
 
-let clearCanvas = document.querySelector('#clearOptionG').addEventListener('click', () => {
+let clearCanvas = document.querySelector('#clear-option-g').addEventListener('click', () => {
     canvasColor = '#f6f6f6';
     for (let l=0; l<lastCycle; l++) {
         setCanvasC(canvasColor, l);
 }});
 
-let randomCanvas = document.querySelector('#randomOptionG').addEventListener('click', () => {
+let randomCanvas = document.querySelector('#random-option-g').addEventListener('click', () => {
     randomizeColor();
     canvasColor = newColor;
     console.log(canvasColor);
@@ -219,23 +162,3 @@ function setCanvasC(colorC, k) {
     let gridDivSelect = document.getElementById(k);
     gridDivSelect.style.cssText = `background-color: ${colorC}`;
 }
-
-// function setInk(colorI, tile) {
-//     let gridSelect = document.getElementById(tile);
-//     gridSelect.style.cssText = `background-color: ${colorI}`;
-// }
-
-// function setInk(colorI) {
-//     for (let j=0; j<lastCycle; j++) {
-//         let gridDivSelect = document.getElementById(j);
-//         gridDivSelect.addEventListener('mouseover', function() {
-//             gridDivSelect.style.cssText = `background-color: ${colorI}`;
-//         });  
-// }}
-
-// function setInk(colorI, k) {
-//     inkColor = '#000000';
-//     for (let j=0; j<lastCycle; j++) {
-//     setInk(inkColor, j);
-// }};
-
